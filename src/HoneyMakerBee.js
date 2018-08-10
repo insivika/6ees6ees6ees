fb6d2cc1c3761a3fa37f53accfe52b6f63e662cb
Bijan Albrecht <Bijanalbrecht@PAlbrecht-W10L.mmreibc.prv>
2018-08-09 19:05:53 -0700

    Completed setting up the classes for Grub, Bee, ForagerBee and HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee{
+  constructor(color, food){
+    super(color, food);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney(){
+    this.honeyPot++;
+  }
+
+  giveHoney(){
+    this.honeyPot--;
+  }
 };
